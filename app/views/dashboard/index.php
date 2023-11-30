0a6757b1fb47108db157cce484a59471595e9969
SHOFYOLA-SI3 <151762741+SHOFYOLA-SI3@users.noreply.github.com>
2023-11-30 23:28:02 +0700

    latihan1
    latihan

---
+++ app/views/dashboard/index.php
@@ -0,0 +1,8 @@
+<h2>Dashboard</h2>
+
+<div class="info"><b>HALLO, SELAMAT DATANG DI WEBSITE PLN SUKA MAJU.ADA YANG INGIN KAMU CARI??</b></div>
+
+<header>
+    <img src="img/PLN2.jpg" class="home" style="width: 400px; height: 350px;">
+    <div class="info1"><b>MARI HIDUP DENGAN TERANGAN AGAR HIDUP SENANTIASA BERJALAN</div>
+</header>
